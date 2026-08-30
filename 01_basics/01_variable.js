fb4c773cce6ebadd72d675c886990019d8bd8c44
Prerana Kushwah <preranakushwah38@gmail.com>
2026-08-30 10:05:18 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 14224
+let accountEmail = "prerana@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 34224
+
+accountEmail = "ores@gmail.com"
+accountPassword = "342863"
+accountCity = "MadhyaPradesh"
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+//alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3
+    +
+    3)// code readability should be high
+
+console.log("prerana")
+
+
+let name = "prerana"
+let age = 45
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+//boolean => true/false
+//null => standalone value
+//underfined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof name);
