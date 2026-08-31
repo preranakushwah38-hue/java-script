80a868fa133eb6fb5cfbc87e34519eabab45c0dc
Prerana Kushwah <preranakushwah38@gmail.com>
2026-08-31 08:52:38 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = true
+
+console.log(typeof score);
+
+let valueInNumber = Boolean(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn)
+
+// 1 =>  true; 0=> false
+// "" =>false
+//"prerana" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
