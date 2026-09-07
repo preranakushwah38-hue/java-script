3f20307568e0c9c25e6b2a0c14007bb57fc7fd2b
Prerana Kushwah <preranakushwah38@gmail.com>
2026-09-07 05:19:20 +0000

    strings

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,4 +3,35 @@ const repoCount = 50
 
 //console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('hitesh-hc-prerana-com')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('s'));
+// console.log(gameName.replace("hiteshc","javascript"));
+const newString = gameName.substring(0,)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 3)
+console.log(anotherString);
+
+const newStringOne = "_____prerana   "
+console.log(newStringOne.trimStart());
+
+
+const url=  "https://upgraded-meme-v69jr%4456vwqfxvq9.github.dev/"
+newurl = url.replace('%44', '-')
+console.log(newurl);
+
+console.log(url.includes('prttana'));
+console.log(gameName.split('h'));
+
+
+
+
