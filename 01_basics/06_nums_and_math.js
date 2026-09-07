3e22e219d94dfdc6fb11c376bca12e844ec42125
Prerana Kushwah <preranakushwah38@gmail.com>
2026-09-07 06:07:01 +0000

    Math and Num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,48 @@
+const score = 400
+// console.log(score);
+
+
+// const balance = new Number(100.09989)
+// console.log(balance);
+
+// console.log(typeof(balance.toString()));
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.54334
+
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++ Maths +++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.52));
+// console.log(Math.ceil(4.38));
+// console.log(Math.floor(4.93));
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(2,2));
+// console.log(Math.min(5,3,32,63,3,54,));
+// console.log(Math.max(5,6,4,34,3,2,54));
+
+console.log(Math.random())
+console.log((Math.random() * 10)+ 1);
+
+console.log(Math.floor(Math.random() * 10)+ 1);
+
+const min  = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max -min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
